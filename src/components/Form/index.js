67f36c0791d50cfa07bc170ad62d2e2510bf727c
geici <geicianecosta43@gmail.com>
2022-07-06 08:54:01 -0400

    testing form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -40,60 +40,31 @@ function ContactForm() {
 
   return (
     <>
-      {/* < Nav /> */}
-      <section id="outer">
-
-
-        <div id="form-page">
-          <div className="myForm">
-            <section id="white-back">
-              <h5 style={{
-                textAlign: "center",
-                fontWeight:"bold",
-                color:"#512b73",
-                marginTop:"10px"
-              }}>Let's Talk!</h5>
-              <form id="form" className="form-style">
-                <img style={{ width: "300px", alignSelf:"start" }}
-                 src={formImg} alt="girl sitting with laptop" />
-                <div className='input-form'>
-                  <div>
-                    {/* <label htmlFor="name">Name:</label> */}
-                    
-                    <input placeholder="NAME"className="form-input" type="text" 
-                    
-                  name="name"   defaultValue={name} onBlur={handleChange} />
-                 
-                  </div>
-                  <span class="dot"></span> 
-                  <div>
-                    {/* <label htmlFor="email">Email address:</label> */}
-                    <input placeholder="EMAIL"className="form-input" type="email"
-                    name="email" defaultValue={email} onBlur={handleChange} />
-                  </div>
-
-
-                  <div>
-                    {/* <label htmlFor="message">Message:</label> */}
-                   
-                    <textarea  placeholder="MESSAGE"className="form-input" name="message" rows="5"
-                     defaultValue={message} onBlur={handleChange} />
-                  </div>
-                  {errorMessage && (
-                    <div>
-                      <p className="error-text">{errorMessage}</p>
-                    </div>
-                  )}
-                  <div className="submit">
-                    <button id="btn-submit" className="btn btn-light form-button"
-                     data-testid="button" type="submit">Submit</button>
-                  </div>
-                </div>
-              </form>
-            </section>
-          </div>
-        </div>
-      </section>
+      < Nav />
+      <div class="wrapper">
+			<div class="inner">
+				<div class="image-holder">
+					<img src={formImg} alt="" />
+				</div>
+				<form action="">
+					<h3>Sign Up</h3>
+					<div class="form-holder active">
+						<input type="text" placeholder="name" class="form-control"/>
+					</div>
+					<div class="form-holder">
+						<input type="text" placeholder="e-mail" class="form-control"/>
+					</div>
+					<div class="form-holder">
+						<input type="password" placeholder="Password" class="form-control" style={{fontSize: "15px"}}/>
+					</div>
+					<div class="checkbox">
+					
+					</div>
+					<div class="form-login">
+					</div>
+				</form>
+			</div>
+		</div>
       <Footer />
     </>
   );
