55abfc6a50f0b4787bd5f70f70bac978cac7d9c4
geici <geicianecosta43@gmail.com>
2022-07-06 16:24:50 -0400

    working on form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -9,7 +9,7 @@ import formImg from '../../assets/images/form-img.jpg'
 import { Placeholder } from 'react-bootstrap';
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
+  const [isActive, setIsActive] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const { name, email, message } = formState;
 
@@ -38,33 +38,50 @@ function ContactForm() {
     }
   };
 
+  const handleClick = event => {
+    // 👇️ toggle isActive state on click
+    setIsActive(current => !current);
+  };
   return (
     <>
       < Nav />
       <div class="wrapper">
-			<div class="inner">
-				<div class="image-holder">
-					<img src={formImg} alt="" />
-				</div>
-				<form action="">
-					<h3>Sign Up</h3>
-					<div class="form-holder active">
-						<input type="text" placeholder="name" class="form-control"/>
-					</div>
-					<div class="form-holder">
-						<input type="text" placeholder="e-mail" class="form-control"/>
-					</div>
-					<div class="form-holder">
-						<input type="password" placeholder="Password" class="form-control" style={{fontSize: "15px"}}/>
-					</div>
-					<div class="checkbox">
-					
-					</div>
-					<div class="form-login">
-					</div>
-				</form>
-			</div>
-		</div>
+        <div class="inner">
+          <div class="image-holder">
+            <img src={formImg} alt="" />
+          </div>
+          <form action="">
+            <h3 id="talk">Let's Talk!</h3>
+
+
+
+
+            <div class="form-holder"
+              className={isActive ? 'form-holder active' : 'form-holder'}
+              onClick={handleClick}>
+              <input type="text" placeholder="name" class="form-control" />
+            </div>
+
+            <div class="form-holder"
+              className={isActive ? 'form-holder active' : 'form-holder'}
+            >
+              <input type="text" placeholder="e-mail" class="form-control" />
+            </div>
+            <div class="form-holder"
+              className={isActive ? 'form-holder active' : 'form-holder'}
+            >
+
+              <input type="text" placeholder="Message" class="form-control" style={{
+                fontSize: "15px",
+                height: "120px"
+              }} />
+            </div>
+            <div className="submit">
+            <button id="btn-submit" className="btn btn-light" data-testid="button" type="submit">Send</button>
+          </div>
+          </form>
+        </div>
+      </div>
       <Footer />
     </>
   );
