fbc31b1ac22fe56bfdb72f26e5d337477939ecb9
geici <geicianecosta43@gmail.com>
2022-07-06 21:46:23 -0400

    form functionality and stuff

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,45 +1,49 @@
 
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+
 import { validateEmail } from '../../utils/helpers';
 import css from './form.css'
 import Nav from '../nav/nav';
 import Footer from '../footer';
 import formImg from '../../assets/images/form-img.jpg'
-import { Placeholder } from 'react-bootstrap';
+import emailjs from 'emailjs-com'
+
 function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-  const [isActive, setIsActive] = useState(false);
+  const form = useRef();
+  // const [formState, setFormState] =
+  //   useState({ name: '', email: '', message: '' });
+  // const { name, email, message } = formState;
   const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
+  const [isActive, setIsActive] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!errorMessage) {
-      setFormState({ [e.target.name]: e.target.value });
-      console.log('Form', formState);
-    }
+   
+    emailjs.sendForm('service_gi9f1ys', 
+    'template_zybg72b', form.current,
+     'tjWNs6WMdJZ0tz0Y5')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
   };
+   
+    
+      // .send(
+      //   'service_gi9f1ys',
+      //   'template_zybg72b',
+      //   formState,
+      //   'tjWNs6WMdJZ0tz0Y5'
+      // )
+      
+    
+  
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-  };
 
   const handleClick = event => {
-    // 👇️ toggle isActive state on click
     setIsActive(current => !current);
   };
   return (
@@ -50,35 +54,46 @@ function ContactForm() {
           <div class="image-holder">
             <img src={formImg} alt="" />
           </div>
-          <form action="">
+          <form ref={form}
+           onSubmit={handleSubmit} action="">
             <h3 id="talk">Let's Talk!</h3>
-
-
-
-
             <div class="form-holder"
               className={isActive ? 'form-holder active' : 'form-holder'}
               onClick={handleClick}>
-              <input type="text" placeholder="name" class="form-control" />
+
+              <input type="text"
+                // onChange={handleChange}
+                name="name"
+                placeholder="name"
+                class="form-control" />
             </div>
 
             <div class="form-holder"
               className={isActive ? 'form-holder active' : 'form-holder'}
             >
-              <input type="text" placeholder="e-mail" class="form-control" />
+              <input type="email"
+                name="email"
+              
+                placeholder="e-mail"
+                // onChange={handleChange}
+                class="form-control" />
             </div>
             <div class="form-holder"
               className={isActive ? 'form-holder active' : 'form-holder'}
             >
 
-              <input type="text" placeholder="Message" class="form-control" style={{
-                fontSize: "15px",
-                height: "120px"
-              }} />
+              <input type="text"
+                name="message" placeholder="Message"
+             
+                // onChange={handleChange}
+                class="form-control" style={{
+                  fontSize: "15px",
+                  height: "120px"
+                }} />
             </div>
             <div className="submit">
-            <button id="btn-submit" className="btn btn-light" data-testid="button" type="submit">Send</button>
-          </div>
+              <button id="btn-submit" className="btn btn-light" data-testid="button" type="submit">Send</button>
+            </div>
           </form>
         </div>
       </div>
